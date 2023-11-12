052fc5822e8f17eb1c1bd70b63a5a696a30b2617
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-11-12 08:28:23 -0500

    The DAL folder and the Entity and Ticket classes are created, the DataBaseContext class is created, the connection string is created.

---
+++ TicketingSystemLaBichota/DAL/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystemLaBichota.DAL.Entities;
+
+namespace TicketingSystemLaBichota.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t  => t.TicketId).IsUnique();
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ TicketingSystemLaBichota/DAL/Entities/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketingSystemLaBichota.DAL.Entities
+{
+    public class Ticket : Entity
+    {
+        [Display(Name = "Ingrese su Ticket")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int TicketId { get; set; }
+
+        [Display(Name = "Fecha de Uso")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "Ticket usado?")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Porteria de Entrada")]
+        public Entrance? EntranceGate { get; set; }
+
+        public enum Entrance
+        {
+            Oriental,
+            Occidental,
+            Norte,
+            Sur,
+        }
+    }
+}
