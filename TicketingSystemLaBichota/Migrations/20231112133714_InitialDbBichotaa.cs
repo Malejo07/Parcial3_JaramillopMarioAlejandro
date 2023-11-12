f95cb6691e2b276c6a9afa573a612f468ed497ca
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-11-12 08:48:43 -0500

    First migrations are added, view is created for tickets Controller

---
+++ TicketingSystemLaBichota/Migrations/20231112133714_InitialDbBichotaa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketingSystemLaBichota.Migrations
+{
+    public partial class InitialDbBichotaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TicketId = table.Column<int>(type: "int", nullable: false),
+                    UseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    EntranceGate = table.Column<int>(type: "int", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_TicketId",
+                table: "Tickets",
+                column: "TicketId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
